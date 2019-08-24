6c30b4068e7064ccda2f712e3c8347340c7c0087
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2019-08-23 22:21:29 -0400

    Completed main game loop

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def generate_player_list(player_count):
     if tie_break_loops == 0:    # initial spin not tied
         n = initial_spins.index(max_spin)
     else:
-        n = [k for (k,v) in curr_cats.items() if v == max_spin][0]
+        n = [k for (k,v) in tie_break_spins.items() if v == max_spin][0]
     
     
     # color offset
@@ -163,7 +163,7 @@ def attr_dict(list_name, attr_name):
 # initialize variables
 #------------------------------------------------------------------------------
 player_count = 4
-total_games = 10000       # number of games to simulate
+total_games = 10       # number of games to simulate
 path_th = 10             # threshold for entering the branched path
 choose_highest = True    # when given the option to take from/give to a player, 
                          # choose the player with the most/fewest) cats
@@ -187,6 +187,7 @@ for game_nbr in range(total_games):
     m = 0    # move
     p = 0    # player
     game_over = False
+    continue_turn = False
     
     # generate a list of players for this game
     players = generate_player_list(player_count)
@@ -195,59 +196,68 @@ for game_nbr in range(total_games):
     moves = []
     
     while game_over != True:
-    
-        # move to the next player
-        if m == 0:
-            p = 0
-        else:
-            p += 1
-            
-        # advance the round number
-        if m > 0:
-            if p == 0 and moves[m].player_nbr != p:
-                r += 1
         
-        # does this player skip a turn?
-        if players[p].skip_next_turn == True:
-            players[p].skip_next_turn = False    # reset the flag
-            continue    # skip turn
-                
-        moves.append(move())
-        moves[m].game_nbr = g
-        moves[m].round_nbr = r
-        moves[m].move_nbr = m
-        moves[m].player_nbr = p
-        moves[m].spin_value = spin()
-        moves[m].player_prev_move_nbr = players[p].prev_move_nbr
-        moves[m].player_prev_location = players[p].location
-    
-        players[p].prev_move_nbr = m
 
+        
+        if continue_turn == False:
+            # if it is not a redirect, initialize the move and get the
+            # landing space
             
-        # find the landing space
-        if players[p].location + moves[m].spin_value <= 28:
-            # before the split
-            moves[m].landing_space = players[p].location + moves[m].spin_value
-                                              
-        elif (players[p].location <= 28 and players[p].cats < path_th) \
-             or (players[p].location > 28 and players[p].location <= 33):
-            # player doesn't have enough cats for lower path OR
-            # player is already on the upper path --> take upper path
-            moves[m].landing_space = (players[p].location + moves[m].spin_value - 1) \
-                                     % 33 + 1
-                                              
-        else:    # enough cats for lower path OR already on lower path
-            moves[m].landing_space = players[p].location + moves[m].spin_value
-            if player[p].location <= 28:
-                moves[m].landing_space += 5
+            # move to the next player
+            if m == 0:
+                p = 0
+            else:
+                p =  (p + 1) % 4
             
-            if moves[m].landing_space > 40:
-                moves[m].landing_space = 40
+            # advance the round number
+            if m > 0:
+                if p == 0 and moves[m-1].player_nbr != p:
+                    r += 1
+                
+            # does this player skip a turn?
+            if players[p].skip_next_turn == True:
+                players[p].skip_next_turn = False    # reset the flag
+                continue    # skip turn
+                    
+            moves.append(move())
+            moves[m].game_nbr = g
+            moves[m].round_nbr = r
+            moves[m].move_nbr = m
+            moves[m].player_nbr = p
+            moves[m].spin_value = spin()
+            moves[m].player_prev_move_nbr = players[p].prev_move_nbr
+            moves[m].player_prev_location = players[p].location
+        
+            players[p].prev_move_nbr = m
+            print(moves[m].spin_value)
+    
+                
+            # find the landing space
+            if players[p].location + moves[m].spin_value <= 28:
+                # before the split
+                moves[m].landing_space = players[p].location + moves[m].spin_value
+                                                  
+            elif (players[p].location <= 28 and players[p].cats < path_th) \
+                 or (players[p].location > 28 and players[p].location <= 33):
+                # player doesn't have enough cats for lower path OR
+                # player is already on the upper path --> take upper path
+                moves[m].landing_space = (players[p].location + moves[m].spin_value - 1) \
+                                         % 33 + 1
+                                                  
+            else:    # enough cats for lower path OR already on lower path
+                moves[m].landing_space = players[p].location + moves[m].spin_value
+                if player[p].location <= 28:
+                    moves[m].landing_space += 5
+                
+                if moves[m].landing_space > 40:
+                    moves[m].landing_space = 40
+
 
         # update the player location
         players[p].location = moves[m].landing_space
+        print(players[p].location)
 
-             
+                 
         # initialize cat counts based on the prior move         
         if m == 0:    # first move
             moves[m].game_tray_cats = 50 - 2*player_count
@@ -260,13 +270,14 @@ for game_nbr in range(total_games):
         # check for player(s) on the same space (take one of their cats)
         for i in range(player_count):
             if i != p \
+               and moves != 0 \
                and players[i].location == players[p].location \
                and players[i].cats >= 0:
                    
                 players[i].cats -= 1
                 players[p].cats += 1
       
-            
+        
         # -- take action based on the space -----------------------------------
         
         # start space or branch point (do nothing)
@@ -300,7 +311,8 @@ for game_nbr in range(total_games):
             
             players[p].cats += c
             moves[m].game_tray_cats -= c
-            
+            print(attr_list(players, 'cats'))
+            print(moves[m].game_tray_cats)
  
         # spaces where the player loses to the game tray
         elif moves[m].landing_space == 26:
@@ -361,7 +373,7 @@ for game_nbr in range(total_games):
             moves[m].animal_shelter_cats += c
             
             #add new move
-            end_move(moves[m])
+            end_move(moves[m], players)
             m += 1
                         
             moves.append(move())
@@ -372,31 +384,29 @@ for game_nbr in range(total_games):
             moves[m].spin_value = ''
             moves[m].player_prev_move_nbr = players[p].prev_move_nbr
             moves[m].player_prev_location = players[p].location
+            moves[m].landing_space = 1
             
             players[p].prev_move_nbr = m
-            players[p].location = 1
-
-      GoTo select_move_action
-
+            continue_turn = True   
+        
 
         # spin again
-        elif moves[m].location in [6, 13, 24, 31]:
-          GoTo player_spin
+        elif moves[m].landing_space in [6, 13, 24, 31]:
+            end_move(moves[m], players)
+            p -= 1
     
     
         # lose next turn
-        elif moves[m].location in [8, 22, 37]:
+        elif moves[m].landing_space in [8, 22, 37]:
             # 8 pick catnip
             # 22 stop to pet a cat
             # 37 hairball
         
             players[p].skip_next_turn = True
-           
-            GoTo next_player
 
     
         # take one from any player
-        elif moves[m].location in [11, 17]:
+        elif moves[m].landing_space in [11, 17]:
             # 11 catsit
             # 17 fall in love with neighborhood cat
     
@@ -414,13 +424,13 @@ for game_nbr in range(total_games):
                 del player_nbrs[p]
                 take_from_player = randrange(len(player_nbrs))
               
-            if cats[take_from_player] > 0:
-                player[p].cats += 1
-                player[take_from_player] -= 1
+            if players[take_from_player].cats > 0:
+                players[p].cats += 1
+                players[take_from_player] -= 1
               
         
         # take one cat from each player
-        elif moves[m].location == 30:
+        elif moves[m].landing_space == 30:
             # 30 catnip in your pocket lures cats
 
             for i in range(player_count):
@@ -430,7 +440,7 @@ for game_nbr in range(total_games):
      
       
         # redirect to another space
-        elif moves[m].location in [12, 16, 18, 21, 29, 34]
+        elif moves[m].landing_space in [12, 16, 18, 21, 29, 34]:
             # 12 kitty litter emergency (supermarket)
             # 16 forgot cat food coupons (return to start)
             # 18 pursue a mangy cat (park)
@@ -438,7 +448,7 @@ for game_nbr in range(total_games):
             # 29 hear the mews of caged cats (animal shelter)
             # 34 forgot flea collars (vet)
     
-            end_move(moves[m])
+            end_move(moves[m], players)
             m += 1
             
             # add new move
@@ -454,25 +464,24 @@ for game_nbr in range(total_games):
             players[p].prev_move_nbr = m
             
             # set the new location
-            if moves[m-1].location == 12:
-                moves[m].location = 20    # supermarket
-            elif moves[m-1].location == 16:
-                moves[m].location = 1     #start
-            elif moves[m-1].location == 18:
-                moves[m].location == 9    # park
-            elif moves[m-1].location == 21:
-                moves[m].location == 26    # vet                
-            elif moves[m-1].location == 29:
-                moves[m].location == 33    # animal shelter
-            elif moves[m-1].location == 34:
-                moves[m].location == 26    # vet
+            if moves[m-1].landing_space == 12:
+                moves[m].landing_space = 20    # supermarket
+            elif moves[m-1].landing_space == 16:
+                moves[m].landing_space = 1     #start
+            elif moves[m-1].landing_space == 18:
+                moves[m].landing_space == 9    # park
+            elif moves[m-1].landing_space == 21:
+                moves[m].landing_space == 26    # vet                
+            elif moves[m-1].landing_space == 29:
+                moves[m].landing_space == 33    # animal shelter
+            elif moves[m-1].landing_space == 34:
+                moves[m].landing_space == 26    # vet
         
-        
-              GoTo select_move_action
-
+            continue_turn = True   
+       
 
         # wildcat card
-        elif moves[m].location == 25:
+        elif moves[m].landing_space == 25:
             
             # select a card at random (they are shuffled prior to each use)
             n = randrange(0,4)
@@ -526,64 +535,46 @@ for game_nbr in range(total_games):
                     player[give_to_player] += 1
                   
     
-
-    # next player
-    
-    # next round        
-    r += 1    
-
-    
-        '--- end game ------------------------------
-        ElseIf moves(6, total_moves - moves_break) = 40 Then
-          'find highest count
-          c = 0
-          j = 0    'max
-          For i = 1 To 4
-            If moves(6 + player_nbr, total_moves - moves_break) > j Then
-              j = moves(6 + player_nbr, total_moves - moves_break)
-            End If
-          Next i
-    
-          'mark the winner(s)
-          For i = 1 To 4
-            If moves(6 + i, total_moves - moves_break) = j Then
-              player_status(8, i) = 1
-            Else
-              player_status(8, i) = 0
-            End If
-          Next i
-          GoTo end_game
+        # end game
+        elif moves[m].landing_space == 40:
+#            'find highest count
+#            c = 0
+#            j = 0    'max
+#            For i = 1 To 4
+#              If moves(6 + player_nbr, total_moves - moves_break) > j Then
+#                j = moves(6 + player_nbr, total_moves - moves_break)
+#              End If
+#            Next i
+#    
+#            'mark the winner(s)
+#            For i = 1 To 4
+#              If moves(6 + i, total_moves - moves_break) = j Then
+#                player_status(8, i) = 1
+#              Else
+#                player_status(8, i) = 0
+#              End If
+#            Next i
+#          
+            game_over = True  
 
 
-
-
+        end_move(moves[m], players)
+        m += 1
 
   
-          end_move(move[m], players)
-
-  
-
-
-      
-        End If
-          
-    next_player:
-      Next player_nbr
-    Loop    'round
-      
-    end_game:
-    'copy ending player status
-    ReDim Preserve end_player_status(1 To 10, 1 To game_nbr * 4)
-    For j = 1 To 4
-      For i = 1 To 10
-        If i = 10 Then
-          end_player_status(i, (game_nbr - 1) * 4 + j) = game_nbr
-        Else
-          end_player_status(i, (game_nbr - 1) * 4 + j) = player_status(i, j)
-        End If
-      Next i
-    Next j
-          
+#end_game:
+#'copy ending player status
+#ReDim Preserve end_player_status(1 To 10, 1 To game_nbr * 4)
+#For j = 1 To 4
+#  For i = 1 To 10
+#    If i = 10 Then
+#      end_player_status(i, (game_nbr - 1) * 4 + j) = game_nbr
+#    Else
+#      end_player_status(i, (game_nbr - 1) * 4 + j) = player_status(i, j)
+#    End If
+#  Next i
+#Next j
+#      
 #  
 #If game_nbr Mod games_break = 0 Then    'if the game number is an increment of the break
 #  file_count = Int((game_nbr - 1) / games_break) + 1
